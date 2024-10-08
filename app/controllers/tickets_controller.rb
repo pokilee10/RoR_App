6a9572f8e6cad8e410f8ea04648284b6fc934856
Lê Xuân Linh <113287392+pokilee10@users.noreply.github.com>
2024-10-08 15:55:01 +0700

    fix send message to Slack

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -23,28 +23,53 @@ class TicketsController < ApplicationController
   end
 
 
+  # def send_to_slack(ticket)
+  #   slack_webhook_url = ENV['SLACK_WEBHOOK_URL']
+  #   message = {
+  #     text: "New Ticket Received:\n Name: #{ticket.name}\n Email: #{ticket.email}\n Description: #{ticket.description}"
+  #   }
+  
+  #   require 'net/http'
+  #   require 'uri'
+  #   require 'json'
+  
+  #   uri = URI.parse(slack_webhook_url)
+  #   request = Net::HTTP::Post.new(uri)
+  #   request.content_type = "application/json"
+  #   request.body = message.to_json
+  
+  #   response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+  #     http.request(request)
+  #     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  #   end
+  
+  #   # Kiểm tra xem phản hồi có thành công không
+  #   response.is_a?(Net::HTTPSuccess)
+  # end
   def send_to_slack(ticket)
     slack_webhook_url = ENV['SLACK_WEBHOOK_URL']
+    uri = URI.parse(slack_webhook_url)
+    
     message = {
       text: "New Ticket Received:\n Name: #{ticket.name}\n Email: #{ticket.email}\n Description: #{ticket.description}"
     }
-  
-    require 'net/http'
-    require 'uri'
-    require 'json'
-  
-    uri = URI.parse(slack_webhook_url)
-    request = Net::HTTP::Post.new(uri)
-    request.content_type = "application/json"
+
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+    http.ca_file = '/etc/ssl/certs/cacert.pem'  # Đường dẫn đến file cacert.pem
+
+    request = Net::HTTP::Post.new(uri.path, {'Content-Type' => 'application/json'})
     request.body = message.to_json
-  
-    response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
-      http.request(request)
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    begin
+      response = http.request(request)
+      Rails.logger.info "Response from Slack: #{response.code} - #{response.message}"
+      return response.is_a?(Net::HTTPSuccess)
+    rescue => e
+      Rails.logger.error "Error sending to Slack: #{e.message}"
+      return false
     end
-  
-    # Kiểm tra xem phản hồi có thành công không
-    response.is_a?(Net::HTTPSuccess)
   end
 end
 
